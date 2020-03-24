07dcf00ebbe1bf702882ccdfe47a048fcf092df2
lukas.sirhal <e1Sil3487>
2020-03-24 18:51:00 +0100

    NL - get total Deseased

--- covid-nl/src/main.js
+++ covid-nl/src/main.js
@@ -1,73 +1,83 @@
 const Apify = require('apify');
+const cheerio = require('cheerio');
 const SOURCE_URL = 'https://www.rivm.nl/node/152921';
 const LATEST = 'LATEST';
-const {log} = Apify.utils;
+const {log, requestAsBrowser} = Apify.utils;
+
+const LABELS = {
+    GOV: 'GOV',
+    WIKI: 'WIKI',
+};
+
+let totalInfected = 0;
+let totalDeceased = undefined;
 
 Apify.main(async () => {
     const requestQueue = await Apify.openRequestQueue();
     const kvStore = await Apify.openKeyValueStore('COVID-19-NL');
     const dataset = await Apify.openDataset("COVID-19-NL-HISTORY");
-    await requestQueue.addRequest({url: SOURCE_URL});
+    await requestQueue.addRequest({ url: SOURCE_URL, userData: { label: LABELS.GOV }});
 
     const crawler = new Apify.CheerioCrawler({
         requestQueue,
         useApifyProxy: true,
         apifyProxyGroups: ['GERMANY'],
         handlePageTimeoutSecs: 120,
-        handlePageFunction: async ({$, body}) => {
-            let totalInfected = 0;
-            let totalDeceased = undefined;
-            const contentDivs = $('.content').toArray();
-            for (const cd of contentDivs) {
-                const contentTitle = $(cd).find('h2');
-                if (contentTitle) {
-                    const text = contentTitle.text().trim();
-                    if (text.includes('Current news')) {
-                        totalInfected = $(cd).find('h4').text().trim();
-                    }
-                }
-            }
-            const newsTitle = $('h2').toArray();
-            for (const title of newsTitle) {
-                const text = $(title).text().trim();
-                if (text.includes('deceased')) {
-                    const arr = text.split(',');
-                    for (const a of arr) {
-                        if (a.includes('deceased')) {
-                            const match = a.match(/\d+/);
-                            if (match) {
-                                totalDeceased = match[0];
+        handlePageFunction: async ({$, request}) => {
+            const { label } = request.userData;
+            switch (label) {
+                case LABELS.GOV:
+                    const contentDivs = $('.content').toArray();
+                    for (const cd of contentDivs) {
+                        const contentTitle = $(cd).find('h2');
+                        if (contentTitle) {
+                            const text = contentTitle.text().trim();
+                            if (text.includes('Current news')) {
+                                totalInfected = $(cd).find('h4').text().trim();
                             }
                         }
                     }
+                    await requestQueue.addRequest({ url: 'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_the_Netherlands', userData: { label: LABELS.WIKI }});
                     break;
-                }
+                case LABELS.WIKI:
+                    const tableRows = $('table.infobox tr').toArray();
+                    for (const row of tableRows) {
+                        const $row = $(row);
+                        const th = $row.find('th');
+                        if (th) {
+                            const value = $row.find('td');
+                            if (th.text().trim() === 'Deaths') {
+                                totalDeceased = value.text().trim();
+                            }
+                        }
+                    }
+                    const data = {
+                        infected: parseInt(totalInfected, 10),
+                        deceased: parseInt(totalDeceased, 10),
+                        SOURCE_URL,
+                        lastUpdatedAtApify: new Date(new Date().toUTCString()).toISOString(),
+                        readMe: 'https://apify.com/lukass/covid-nl',
+                    };
+
+                    // Compare and save to history
+                    const latest = await kvStore.getValue(LATEST);
+                    if (latest) {
+                        delete latest.lastUpdatedAtApify;
+                    }
+                    const actual = Object.assign({}, data);
+                    delete actual.lastUpdatedAtApify;
+                    await Apify.pushData(data);
+
+                    if (JSON.stringify(latest) !== JSON.stringify(actual)) {
+                        log.info('Data did change :( storing new to dataset.');
+                        await dataset.pushData(data);
+                    }
+
+                    await kvStore.setValue(LATEST, data);
+                    log.info('Data stored, finished.')
+                    break;
             }
 
-            const data = {
-                infected: parseInt(totalInfected, 10),
-                deceased: parseInt(totalDeceased, 10),
-                SOURCE_URL,
-                lastUpdatedAtApify: new Date(new Date().toUTCString()).toISOString(),
-                readMe: 'https://apify.com/lukass/covid-nl',
-            };
-
-            // Compare and save to history
-            const latest = await kvStore.getValue(LATEST);
-            if (latest) {
-                delete latest.lastUpdatedAtApify;
-            }
-            const actual = Object.assign({}, data);
-            delete actual.lastUpdatedAtApify;
-            await Apify.pushData(data);
-
-            if (JSON.stringify(latest) !== JSON.stringify(actual)) {
-                log.info('Data did change :( storing new to dataset.');
-                await dataset.pushData(data);
-            }
-
-            await kvStore.setValue(LATEST, data);
-            log.info('Data stored, finished.')
         },
         handleFailedRequestFunction: async ({request}) => {
             console.log(`Request ${request.url} failed twice.`);
