5762f4de90b16c3a404b8676845ef79880382a86
lukas.sirhal <e1Sil3487>
2020-03-29 12:19:02 +0200

    CAD - init

---
+++ covid-cad/src/main.js
@@ -0,0 +1,87 @@
+const Apify = require('apify');
+const SOURCE_URL = 'https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection.html';
+const LATEST = 'LATEST';
+const {log, requestAsBrowser} = Apify.utils;
+
+Apify.main(async () => {
+    const requestQueue = await Apify.openRequestQueue();
+    const kvStore = await Apify.openKeyValueStore('COVID-19-CAD');
+    const dataset = await Apify.openDataset("COVID-19-CAD-HISTORY");
+    await requestQueue.addRequest({ url: 'https://health-infobase.canada.ca/src/data/covidLive/covid19.csv'});
+
+    await Apify.addWebhook({
+        eventTypes: ['ACTOR.RUN.FAILED', 'ACTOR.RUN.TIMED_OUT'],
+        requestUrl: `https://api.apify.com/v2/acts/mnmkng~email-notification-webhook/runs?token=${Apify.getEnv().token}`,
+        payloadTemplate: `{"notificationEmail": "sirhallukas@gmail.com", "eventType": {{eventType}}, "eventData": {{eventData}}, "resource": {{resource}} }`,
+    });
+
+    const crawler = new Apify.BasicCrawler({
+        requestQueue,
+        handleRequestFunction: async ({ request }) => {
+            const proxyUrl = Apify.getApifyProxyUrl({
+                groups: ['SHADER'],
+            });
+
+            const response = await requestAsBrowser({
+                url: request.url,
+                proxyUrl,
+            });
+            const lines = response.body.split(/\r?\n/);
+            let totalInfected = undefined;
+            let totalDeceased = undefined;
+            const infectedByRegion = [];
+            const tempByRegion = [];
+            for (let i = 1; i < lines.length; i++) {
+                const values = lines[i].split(',');
+                tempByRegion[values[0]] =
+                  {
+                      "region": values[1],
+                      "infectedCount": values[4],
+                      "deceasedCount": values[6]
+                  };
+                if (values[0]) {
+                    totalInfected = values[4];
+                    totalDeceased = values[6];
+                }
+            }
+            for (const item of tempByRegion) {
+                if (item) {
+                    infectedByRegion.push(item);
+                }
+            }
+
+            const data = {
+                infected: parseInt(totalInfected, 10),
+                deceased: parseInt(totalDeceased, 10),
+                infectedByRegion,
+                SOURCE_URL,
+                lastUpdatedAtApify: new Date(new Date().toUTCString()).toISOString(),
+                readMe: 'https://apify.com/lukass/covid-cad',
+            };
+
+            // Compare and save to history
+            const latest = await kvStore.getValue(LATEST);
+            if (latest) {
+                delete latest.lastUpdatedAtApify;
+            }
+            const actual = Object.assign({}, data);
+            delete actual.lastUpdatedAtApify;
+            await Apify.pushData(data);
+
+            if (JSON.stringify(latest) !== JSON.stringify(actual)) {
+                log.info('Data did change :( storing new to dataset.');
+                await dataset.pushData(data);
+            }
+
+            await kvStore.setValue(LATEST, data);
+            log.info('Data stored, finished.')
+        },
+        handleFailedRequestFunction: async ({request}) => {
+            console.log(`Request ${request.url} failed twice.`);
+        },
+    });
+
+    log.info('CRAWLER -- start');
+    await crawler.run();
+    log.info('CRAWLER -- finish');
+});
