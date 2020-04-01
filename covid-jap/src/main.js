cd817a89a18f7a1f2a93db6abb8470fd3a949ec7
lukas.sirhal <e1Sil3487>
2020-04-01 13:55:51 +0200

    JAP add log info

--- covid-jap/src/main.js
+++ covid-jap/src/main.js
@@ -101,6 +101,10 @@ Apify.main(async () => {
     }
     if (latest.infected > data.infected || latest.deceased > data.deceased) {
         log.error('Latest data are high then actual - probably wrong scrap');
+        log.info('ACTUAL DATA');
+        console.log(data);
+        log.info('LATEST DATA');
+        console.log(latest);
         process.exit(1);
     }
     const actual = Object.assign({}, data);
